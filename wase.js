8d1f22f5fd52d430e43fc9171f225628606725f9
Waseem <waseembassamayesh@gmail.com>
2017-02-19 05:47:15 -0800

    add file npm

--- wase.js
+++ wase.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function(req,res){
   res.end('Hello my name is Waseem');
-}).listen(8080,function(){
+}).listen(process.env.PORT,function(){
   console.log('Listening on 8080');
-});
+}); 
